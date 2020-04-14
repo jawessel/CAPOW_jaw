d94232a7732c8e1a6390370d890ecab89ba3b595
Jordan Kern <jkern@ncsu.edu>
2020-04-14 01:52:14 -0400

    update

--- Stochastic_engine/stochastic_engine.py
+++ Stochastic_engine/stochastic_engine.py
@@ -55,7 +55,7 @@ print('streamflows')
 # DAILY HYDROPOWER SIMULATION
 
 # Now specify a smaller subset of stochastic data to run (must be <= stoch years)
-sim_years = 1
+sim_years = 4
 
 # Run ORCA to get California storage dam releases
 import main
